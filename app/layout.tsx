566456b770e9308d7e40b2e597d548ab3dc6c3d4
D.VAMSI TEJ CHOWDARY <d.vamsitej333@gmail.com>
2025-03-05 16:51:23 -0500

    Added favicon image with letter v

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.png" sizes="any" />
+        <link rel="icon" href="/letter-v.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
